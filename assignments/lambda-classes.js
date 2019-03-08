79cdb45f737b046845bd9377f8613d0a6839644b
anthony <anthony24.kwhomes@gmail.com>
2019-03-07 16:24:37 -0800

    First project commit

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,38 @@
 // CODE here for your Lambda Classes
+
+const Barry = new person({
+    name: 'Barry',
+    location: 'Central City',
+    age: 25,
+    gender: 'male'
+});
+
+const Hal = new person({
+    name: 'Hal',
+    location: 'Coast City',
+    age: 30,
+    gender: 'male'
+})
+
+const Arthur = new Instructor({
+    name: 'Arthur',
+    location: 'Atlantis',
+    age: 30,
+    gender: 'male',
+    favLanguage: 'Python',
+    specialty: 'Front-end',
+    catchPhrase: 'Great Atlantis!'
+
+});
+
+const Diana = new Instructor({
+    name: 'Diana',
+    location: 'Themyscira',
+    age: 30,
+    gender: 'female',
+    favLanguage: 'Java',
+    specialty: 'Back end',
+    catchPhrase: 'I will fight for those who cannot fight for themselves'
+});
+
+    
